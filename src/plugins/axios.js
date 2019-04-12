8dc4c7c9c78c9bec8b5d4571ec6de510a4b58646
Abbey <i.olasunkanmi@dreammesh.ng>
2019-04-12 22:54:39 +0100

    Configured axios and set the baseURL

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,15 +3,12 @@
 import Vue from 'vue';
 import axios from "axios";
 
-// Full config:  https://github.com/axios/axios#request-config
-// axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
-// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
 
 let config = {
-  // baseURL: process.env.baseURL || process.env.apiUrl || ""
-  // timeout: 60 * 1000, // Timeout
-  // withCredentials: true, // Check cross-site Access-Control
+  
 };
 
 const _axios = axios.create(config);
