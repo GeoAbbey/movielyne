a5128ec4a1b581e517d1bcf9de93d5742ccbc6db
Abbey <i.olasunkanmi@dreammesh.ng>
2019-04-13 06:01:16 +0100

    Added more comments

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-const KEY = "c120980454891f98e1fc45b68661d5ec";
-// const KEY = "xxxxxx API_KEY xxxxxxx";
-
+const KEY = "xxxxxx API_KEY xxxxxxx";
 
 export default {
+  // Fetch popular movies
   getPopularMovies(pageNo) {
     return axios
       .get(`/movie/popular?api_key=${KEY}&page=${pageNo}`)
@@ -13,6 +12,7 @@ export default {
       });
   },
 
+  // Fetch trending movies
   getTrendingMovies(pageNo) {
     return axios
       .get(`trending/movie/week?api_key=${KEY}&page=${pageNo}`)
@@ -21,33 +21,42 @@ export default {
       });
   },
 
+  // Fetch a single movie and its details
   getSingleMovie(id) {
     return axios.get(`/movie/${id}?api_key=${KEY}`).then(response => {
       return response.data;
     });
   },
 
+  // Fetch the casts and crew for a particular movie
   getCredits(id) {
     return axios.get(`/movie/${id}/credits?api_key=${KEY}`).then(response => {
       return response.data;
     });
   },
 
+  // Fetch a single cast and his/her details
   getSingleCast(id) {
     return axios.get(`/person/${id}?api_key=${KEY}`).then(response => {
       return response.data;
     });
   },
 
+  // Fetch all the movies a cast has participated in
   getCastMovies(id) {
-    return axios.get(`/person/${id}/movie_credits?api_key=${KEY}`).then(response => {
-      return response.data;
-    });
+    return axios
+      .get(`/person/${id}/movie_credits?api_key=${KEY}`)
+      .then(response => {
+        return response.data;
+      });
   },
 
+  // Fetch movie(s) based on search entry
   searchMovies(movieName) {
-    return axios.get(`/search/movie?api_key=${KEY}&query=${movieName}`).then(response => {
-      return response.data;
-    });
+    return axios
+      .get(`/search/movie?api_key=${KEY}&query=${movieName}`)
+      .then(response => {
+        return response.data;
+      });
   }
 };
